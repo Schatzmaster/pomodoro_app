bf260cc44d08d6fffa21de5f3c709ebdfd82eb41
jasper_hansen <jasper.hansen@enovetic.de>
2024-09-30 11:11:30 +0200

    Final commit

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from tkinter import *
 import math
 
 
-
-
-def reset_button_clicked():
-    pass
-
-
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -18,24 +12,62 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
-# ---------------------------- TIMER RESET ------------------------------- # 
+timer = None
 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_lable.config(text="Timer")
+    checkmark.config(text="")
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_countdown():
     global reps
-    count_down(5*60)
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # If it's the 1st/3rd/5th/7th rep:
+    if reps % 8 == 0:
+        title_lable.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    # If it's 8th rep:
+    elif reps % 2 == 0:
+        title_lable.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    # If it's 2nd/4th/6th rep:
+    else:
+        title_lable.config(text="Work")
+        count_down(work_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    global reps
+
     if count_sec < 10:
         count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_countdown()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        checkmark.config(text=marks)
+
 
 # After() tracks if something happened after an amount of  millisecond
 # ---------------------------- UI SETUP ------------------------------- #
@@ -51,21 +83,20 @@ canvas.create_image(100, 112, image=tomato_img)  # First values are x and y coor
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-
 # Label
 
 title_lable = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 title_lable.grid(row=0, column=1)
 
-# CHeckmark
-checkmark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, "bold"))
+# Checkmark
+checkmark = Label(text="", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16, "bold"))
 checkmark.grid(row=3, column=1)
 
 # Buttons
 start_button = Button(text="Start", command=start_countdown)
 start_button.grid(row=2, column=0)
 
-reset_button = Button(text="Reset", command=reset_button_clicked)
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(row=2, column=2)
 
 window.mainloop()
